48ae3c4e62320c46670d1b6fe29cea15d8b32dda
Nathan Nye <nathan@nye.design>
2024-10-14 09:57:51 -0400

    logs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import Sector from "./sector";
 export default class App{
   constructor() {
     currentPage = window.location.pathname;
+    console.log('current page', currentPage)
     this.init()
   }
 
@@ -14,18 +15,22 @@ export default class App{
     new Global()
     if (this.currentPage === '/') {
       new Home()
+      console.log('created home')
     }
 
     if(this.currentPage === '/about') {
       new About()
+      console.log('created about')
     }
 
     if (this.currentPage.startsWith('/projects/')) {
       new Project()
+      console.log('created project')
     }
 
     if (this.currentPage.startsWith('/sector/')) {
       new Sector()
+      console.log('created sector')
     }
 
 
