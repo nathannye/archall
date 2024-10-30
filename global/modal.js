319f1e5a2279a984364fd99f1099b17c5d5dd3d0
Nathan Nye <nathan@nye.design>
2024-10-30 10:52:33 -0600

    redefine tl

--- global/modal.js
+++ global/modal.js
@@ -1,18 +1,55 @@
+import gsap from "gsap";
+
 export default class ModalForm{
   constructor(el) {
     this.el = el
     this.isOpen = false
     this.triggers = document.querySelectorAll('[data-modal="trigger"]');
     this.modalContent = this.el.querySelector(".modal-form");
-    
+
     this.init()
   }
 
   init() {
+
+    const tl = gsap.timeline({
+      paused: true,
+    });
+  
+    tl.to(
+      this.modal,
+      {
+        display: "flex",
+        duration: 0,
+        ease: "none",
+      },
+      0
+    )
+      .from(
+        this.el,
+        {
+          opacity: 0,
+          duration: 0.7,
+          ease: "expo.out",
+        },
+        0.2
+      )
+      .to(
+        this.modalContent,
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.7,
+          ease: "expo.out",
+        },
+        0.2
+      );
+
+
     this.triggers?.forEach((trigger) => {
       trigger.addEventListener("click", (e) => {
         e.preventDefault();
-        this.isOpen ? this.tl.reverse() : this.tl.play();
+        this.isOpen ? tl.reverse() : tl.play();
         this.el.setAttribute("data-open", this.isOpen ? "false" : "true");
         this.isOpen = !this.isOpen;
       });
