0a9e73ace578cde6c609a6ba98aba4eca3dcb05c
Nathan Nye <nathan@nye.design>
2025-05-19 19:57:26 -0600

    logs

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,8 +1,10 @@
 export const emit = (eventName, data = {}) => {
 	const event = new CustomEvent(eventName, { detail: data });
+	console.log("emitting event with data::", data, event);
 	document.dispatchEvent(event);
 };
 
 export const listen = (eventName, callback) => {
+	console.log("listening for event::", eventName);
 	document.addEventListener(eventName, callback);
 };
